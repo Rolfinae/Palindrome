93c93853ba99a17c9d61d07b271e4e866ec6231d
Bryan <bryan_kenis@hotmail.com>
2019-01-23 14:09:44 +0100

    initial commi

---
+++ BllPalindrome/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DalPalindrome;
+
+namespace BllPalindrome
+{
+    public class PalindromeManagement
+    {
+        public List<string> getListPalindromeFromFile(string pFileName)
+        {
+            FileTextReaderUtility myReader = new FileTextReaderUtility();
+            List<string> readLines = myReader.ReadAllLines(pFileName);
+            List<string> lineClean = new List<string>();
+            foreach (string line in readLines)
+            {
+                string[] monTableau;
+                line.Trim();
+                monTableau = line.Split(',','(');
+                lineClean.Add(monTableau[0].Trim());
+            }
+            return lineClean;
+        }
+    }
+}
